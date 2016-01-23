11621aba99874dab7c681978d78428b888b74fe1
Ruslan Ibragimov <theliveperson@gmail.com>
2016-01-23 13:40:33 +0300

    Refactoring: implementing lib module.

--- apiary-api-lib/src/main/kotlin/by/heap/apiary/api/lib/ApiaryResponse.kt
+++ apiary-api-lib/src/main/kotlin/by/heap/apiary/api/lib/ApiaryResponse.kt
@@ -1,13 +1,12 @@
-package by.heap.apiary.api
+package by.heap.apiary.api.lib
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.apache.http.HttpEntity
 import org.apache.http.util.EntityUtils
 
 data class ApiaryResponse (val error: Boolean?, val message: String?, val code: String?)
 
-val mapper = ObjectMapper().registerKotlinModule()
+val mapper = jacksonObjectMapper()
 
 fun HttpEntity.toResponse(): ApiaryResponse = mapper.readValue(EntityUtils.toString(this))
